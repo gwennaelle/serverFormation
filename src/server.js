503a6a9b69bdd4e3031fd813d395e455ab0ddac9
groy <41110605+gwennaelle@users.noreply.github.com>
2018-08-07 15:08:34 +0200

    middleware

--- src/server.js
+++ src/server.js
@@ -3,21 +3,38 @@ import fs from 'fs'
 
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+// middleware
+// -------------------------------------------------------------------------------------
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+    next();
 })
 
-app.get('/ping', (req, res) => {
-    res
-    .status(200)
-    .send('pong')
-  })
+app.use(function (req, res, next) {
+    setTimeout(next, 3000);
+})
 
+
+// routes
+// -------------------------------------------------------------------------------------
 app.get('/movies', (req, res) => {
     var json = fs.readFileSync('src/data/movies.json')
     res.send(JSON.parse(json))
 })
 
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+  
+app.get('/ping', (req, res) => {
+    res
+    .status(200)
+    .send('pong')
+})
+
+// demarrage de l'application
+// -------------------------------------------------------------------------------------
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!')
 })
